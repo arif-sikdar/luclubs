0b297326728eb0d28d31821fced61d77cd9cb795
Nabila2002 <nabilazannat20@gmail.com>
2022-12-01 17:09:52 +0600

    register added

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:lu_clubs/register.dart';
+
+
+import 'login.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyLogin(),
+    routes: {
+      'register': (context) => MyRegister(),
+      'login': (context) => MyLogin(),
+    },
+  ));
+}
